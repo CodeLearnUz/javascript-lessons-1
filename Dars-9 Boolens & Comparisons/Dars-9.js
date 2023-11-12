e322b17b2aa1951bd57253bf489f369ac96b630e
DostonLearn <149419882+CodeLearnUz@users.noreply.github.com>
2023-11-12 09:30:51 +0300

    javascript lessons-9
    javascript lessons-9

---
+++ Dars-9 Boolens & Comparisons/Dars-9.js
@@ -0,0 +1,27 @@
+// boolens & comparisons
+
+// console.log(true, false, 'true', 'false');
+
+// methods can return boolens
+// let email = 'dostonweb@gmail.com'
+
+// let result = email.includes('@')
+
+// console.log(result);
+
+// comparisons operators
+
+// let age = 25
+
+// console.log(age == 25); // noqatiy tenglik
+// console.log(age == 30);
+// console.log(age != 30);
+// console.log(age > 20);
+// console.log(age < 20);
+// console.log(age <= 25);
+// console.log(age >= 25);
+
+// let ism = 'Doston'
+// console.log(ism == 'Doston');
+// console.log(ism == 'doston');
+// console.log(ism > 'Doston');
