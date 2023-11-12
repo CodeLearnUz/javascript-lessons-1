dda74ace080af19b64261aac128046e74f6d1a5d
DostonLearn <149419882+CodeLearnUz@users.noreply.github.com>
2023-11-12 09:27:53 +0300

    javascript lessons-2
    javascript lessons-2

---
+++ Dars-2 Variables/Dars-2.js
@@ -0,0 +1,14 @@
+let ism = 'Doston'
+let yosh = 17
+console.log(ism, yosh);
+
+yosh = 25
+
+console.log(yosh);
+
+const familiya = 'Bahriddinov'
+console.log(familiya);
+
+var joy = 'Samarqand'
+joy = 'Samarqand tumani'
+console.log(joy);
