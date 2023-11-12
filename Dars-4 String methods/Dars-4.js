2b1abb385f401927a82cfae8360cb3bc65a90690
DostonLearn <149419882+CodeLearnUz@users.noreply.github.com>
2023-11-12 09:29:00 +0300

    javascript lessons-4
    javascript lessons-4

---
+++ Dars-4 String methods/Dars-4.js
@@ -0,0 +1,49 @@
+//*----- ko'p ishlatiladigan string metodlar -----*//
+
+let email = 'Dostonweb@gmail.com'
+
+// lastIndexOf()
+
+// let result = email.lastIndexOf('o')
+// console.log(result);
+
+// indexOf()
+
+// let result = email.indexOf('a')
+// console.log(result);
+
+// slice()
+
+// let result = email.slice(1, 3)
+// console.log(result);
+
+// substr()
+
+// let result = email.substr(1, 9)
+// console.log(result);
+
+// replace()
+
+// let result = email.replace('D', 'O')
+// console.log(result);
+
+// charAt() birinchi harakterni oladi
+
+// let result = email.charAt()
+// console.log(result);
+
+// toUpperCase() - toLowerCase()
+
+// let result = email.toUpperCase()
+// let result = email.toLocaleLowerCase()
+// console.log(result);
+
+// trim()
+
+// let result = email.trim()
+// console.log(result);
+
+// split()
+
+// let result = email.split()
+// console.log(result);
