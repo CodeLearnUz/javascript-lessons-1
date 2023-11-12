a9bec0083d5f16fba401c6b41e24f672e1597552
DostonLearn <149419882+CodeLearnUz@users.noreply.github.com>
2023-11-12 09:39:48 +0300

    javascript lessons-32
    javascript lessons-32

---
+++ Dars-32 Math Objecti/Dars-32.js
@@ -0,0 +1,26 @@
+// Math object
+
+// Math, Math.PI, Math.E
+
+// console.log(Math);
+// console.log(Math.E);
+// console.log(Math.PI);
+
+// const area = 7.9
+
+// Math.round
+// console.log(Math.round(area));
+
+
+// Math.floor
+// console.log(Math.floor(area));
+
+// Math.ceil
+// console.log(Math.ceil(area));
+
+// Math.trunc
+// console.log(Math.trunc(area));
+
+// Math.random
+// const randomNumber = Math.trunc(Math.random(area) * 10) + 1
+// console.log(randomNumber);
