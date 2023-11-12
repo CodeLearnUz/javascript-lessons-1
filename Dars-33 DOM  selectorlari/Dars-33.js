b0a34fb33067f1cbc92c8b0985971fc3fc762cab
DostonLearn <149419882+CodeLearnUz@users.noreply.github.com>
2023-11-12 09:40:21 +0300

    javascript lessons-33
    javascript lessons-33

---
+++ Dars-33 DOM  selectorlari/Dars-33.js
@@ -0,0 +1,23 @@
+// Dom
+
+// getElementByIdTagName
+
+// const listItem = document.getElementsByTagName('li')
+// console.log(listItem);
+
+// getElementByIdClassName
+// const listItem = document.getElementsByClassName('list-item')
+// console.log(listItem);
+
+// getElementById
+// const clickBtn = document.getElementById('click-btn')
+// console.log(clickBtn);
+
+// querySelector / queryselectorAll
+// const listItem = document.querySelector('.list-item')
+// console.log(listItem);
+
+// ALL
+
+// const listItem = document.querySelectorAll('.list-item')
+// console.log(listItem);
