a6fafb50d3df211ccfbce0c684d01c8cf19b60f6
DostonLearn <149419882+CodeLearnUz@users.noreply.github.com>
2023-11-12 09:31:41 +0300

    javascript lessons-10
    javascript lessons-10

---
+++ Dars-10 Kuchli va Kuchsiz taqqoslash/Dars-10.js
@@ -0,0 +1,15 @@
+let age = 25
+
+// kuchsiz taqqoslashlar (turli xil tiplar tengligi)
+
+// console.log(age == 25);
+// console.log(age == '25');
+// console.log(age != 25);
+// console.log(age != '25');
+
+// kuchli taqqoslashlar (turli xil tiplar teng bo'lmasligi)
+
+// console.log(age === 25);
+// console.log(age === '25');
+// console.log(age !== 25);
+// console.log(age !== '25');
