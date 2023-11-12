0cf33a20a86a2dc7eb68fea692430523af6ec47a
DostonLearn <149419882+CodeLearnUz@users.noreply.github.com>
2023-11-12 09:43:33 +0300

    javascript lessons-42
    javascript lessons-42

---
+++ Dars-42 Array Fillter/Dars-42.js
@@ -0,0 +1,23 @@
+const movies = [
+    {name: 'Avatar 3D', year: 2008, rating: 9},
+    {name: 'Titanik 3D', year: 1997, rating: 7},
+    {name: 'Forsaj 9 3D', year: 2020, rating: 4},
+    {name: 'Avengers', year: 2012, rating: 6},
+]
+
+// const newMovies = []
+
+// movies.forEach((movie) => {
+//     if(movie.year > 2010){
+//         newMovies.push(movie)
+//     }
+// })
+
+// console.log(newMovies);
+
+// const filterMoveies = movies.filter((movie) => {
+//     return movie.year > 2010
+// })
+
+// console.log(filterMoveies);
+
