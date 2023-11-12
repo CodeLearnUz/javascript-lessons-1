6f0d9187490039b7ef6bf1e6029731f2bed8ecc9
DostonLearn <149419882+CodeLearnUz@users.noreply.github.com>
2023-11-12 09:36:03 +0300

    javascript lessons-21
    javascript lessons-21

---
+++ Dars-21 Functions/Dars-21.js
@@ -0,0 +1,23 @@
+// function decloration
+
+function sayHello(){
+    console.log('Hello function decloration');
+}
+sayHello()
+
+
+// function expression
+
+const sayByeBye = function(){
+    console.log('Say byebye');
+}
+
+sayByeBye()
+
+// arrow function
+
+const nice = () => {
+    console.log("this is arrow function");
+}
+
+nice()
