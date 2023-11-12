0fe30cc64dbd138b4011c12e822652909d9608b9
DostonLearn <149419882+CodeLearnUz@users.noreply.github.com>
2023-11-12 09:41:14 +0300

    javascript lessons-35
    javascript lessons-35

---
+++ Dars-35 setAttribute & getAttribute/Dars-35.js
@@ -0,0 +1,9 @@
+// const link = document.querySelector('a')
+// console.log(link.getAttribute('href'));
+
+// link.setAttribute('href', 'https://www.yandex.ru')
+// link.textContent = 'Go To Yandex Miain Page'
+
+// const text = document.querySelector('p')
+// console.log(text.getAttribute('class'));
+// text.setAttribute('class', 'succes')
