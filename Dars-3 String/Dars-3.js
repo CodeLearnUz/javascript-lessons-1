de731985101f8f96be3ccfbdfed5f821d2de6899
DostonLearn <149419882+CodeLearnUz@users.noreply.github.com>
2023-11-12 09:28:33 +0300

    javascript lessons-3
    javascript lessons-3

---
+++ Dars-3 String/Dars-3.js
@@ -0,0 +1,33 @@
+// string
+
+console.log('Hello world +!');
+
+let email = 'dostonweb@gmail.com'
+console.log(email);
+
+// string concatenation
+
+let firstName = 'Doston'
+let lastName = 'Bahriddinov'
+
+let fullName = firstName + ' ' + lastName
+console.log(fullName);
+
+// getting characters
+
+console.log(fullName[2]);
+
+// string length => uzunlik
+
+console.log(fullName.length);
+
+// string methods => funktsiyaga o'xsh ish bajaradi
+
+console.log(fullName.toUpperCase());
+
+let result = fullName.toLocaleLowerCase()
+console.log(result);
+
+
+let index = fullName.indexOf('D')
+console.log(index);
