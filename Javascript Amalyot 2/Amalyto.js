ab518eca98512f32d90d7cb2c54761ea3d77e98b
DostonLearn <149419882+CodeLearnUz@users.noreply.github.com>
2023-11-12 09:45:08 +0300

    javascript amalyot-2
    javascript amalyot-2

---
+++ Javascript Amalyot 2/Amalyto.js
@@ -0,0 +1,66 @@
+// const ism = prompt('ismingizni kiriting:')
+// const yosh = +prompt('yoshingizni kirirting:')
+
+// alert(`Salom ${ism} sizning tug'ulgan yilingiz ${2023 - yosh} va yashagan oyingiz ${yosh * 12}`)
+
+// const ismlar = ['Ahror', 'Sardor', 'Donyor']
+
+// const sora = prompt('ism sorang: ')
+
+// for(let i = 0; i < ismlar.length; i++){
+//     // console.log(ismlar[i]);
+//     if(sora === ismlar[i]){
+//         console.log(`Siz so'ragan ${ismlar[i]} ismi bor`);
+//         break
+//     }
+//     else{
+//         console.log("Siz so'ragn ism yo'q");
+//     break
+//     }
+// }
+
+// const ismlar = ['Ahror', 'Sardor', 'Donyor']
+
+// for(let i = 0; i < ismlar.length; i++){
+//     let result = ismlar[i] + 'bek'
+//     console.log(result);
+// }
+
+// let i = 0
+
+// while(i < ismlar.length){
+//     let result = ismlar[i] + 'bek'
+//     console.log(result);
+//     i++
+// }
+
+
+
+
+// const viloyatNoim = prompt('Qaysi viloyat kerak ?')
+
+// switch(viloyatNoim){
+//     case "Farg'ona":
+//         alert("2 mln aholi")
+//     break
+//     case "Samarqand":
+//         alert("1.5 mln aholi")
+//     break
+//     case "Jizzax":
+//         alert("3.2 mln aholi")
+//     break
+//     default:
+//         alert(`Siz so'ragan ${viloyatNoim} viloyati topilmadi !`)
+// }
+
+// let meyor = 4
+
+// const soz = prompt("So'z kiriting:")
+
+// if(soz.length == meyor){
+//     alert(`Siz ${soz.length} ta so'z kiritingiz bu meyyorida`)
+// }else if(soz.length < meyor){
+//     alert(`Siz ${soz.length} ta so'z kiritingiz bu meyyordan past`)
+// }else{
+//     alert(`Meyyor 20ta edi siz meyyordan ${soz.length - meyor} ta so'z kiritingiz bu meyyordan juda balland`)
+// }
