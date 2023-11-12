3414cdce87c4d04e7d26e038fb83903d53c5abf5
DostonLearn <149419882+CodeLearnUz@users.noreply.github.com>
2023-11-12 09:29:46 +0300

    javascript lessons-6
    javascript lessons-6

---
+++ Dars-6 Template string/Dars-6.js
@@ -0,0 +1,15 @@
+// template string
+// const title = 'Rastamojka qachon ochiladi ?! yohud Monopoliya Respublikas !'
+// const author = "Lag'mon markazi"
+// const likes = 35000000
+
+// concatenation way
+
+// let result = 'Bu post nomi ' + title + ' ' + author + ' tomonidan yozilgan.' + ' va bu postda ' + likes  + ' ta like bor'
+// console.log(result);
+
+// template string way
+
+// let result = `Bu post nomi ${title} ${author} tomonidan yozilga va bu postda ${likes} ta like bor`
+// console.log(result);
+
