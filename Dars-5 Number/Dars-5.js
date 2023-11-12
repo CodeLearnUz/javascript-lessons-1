b738360b14523116ffc02ee2610ad63f456cf45d
DostonLearn <149419882+CodeLearnUz@users.noreply.github.com>
2023-11-12 09:29:25 +0300

    javascript lessons-5
    javascript lessons-5

---
+++ Dars-5 Number/Dars-5.js
@@ -0,0 +1,42 @@
+// let radius = 10
+// const pi = 3.14
+
+// console.log(radius, pi);
+
+// math operators +,-,*,/,**,%
+
+// console.log(10 / 2);
+
+// let result = radius % 3
+// console.log(result);
+
+// let result =  pi * radius ** 2
+// console.log(result);
+
+// order of operation
+
+// let result = 5 * (10 - 3) **2
+// console.log(result);
+
+// let likes = 10
+
+// likes = likes + 1
+// likes++
+// likes--
+// likes = likes + 10
+// likes += 12
+// likes -= 12
+// likes /= 5
+// likes *= 5
+
+// console.log(likes);
+
+// NaN = not a number
+
+// console.log(5 / 'Salom');
+// console.log(5 * 'Salom');
+
+// let likes = 10
+
+// let result = 'the blog has ' + likes + ' likes'
+// console.log(result);
