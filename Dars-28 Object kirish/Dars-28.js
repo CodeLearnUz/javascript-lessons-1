3b1ee5e494381b3a9b8d42413023e15ac90b88f3
DostonLearn <149419882+CodeLearnUz@users.noreply.github.com>
2023-11-12 09:37:49 +0300

    javascript lessons-28
    javascript lessons-28

---
+++ Dars-28 Object kirish/Dars-28.js
@@ -0,0 +1,11 @@
+// object
+
+const user = {
+    name: 'Doston',
+    age: 24,
+    email: 'dostonweb@gmail.com',
+    location: 'Samarqand',
+    lang: ['uzbek', 'russian', 'english']
+} 
+
+console.log(typeof user); 
