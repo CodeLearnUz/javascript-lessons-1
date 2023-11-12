8d0c1852566e075df796959bf27d45da89c1d49e
DostonLearn <149419882+CodeLearnUz@users.noreply.github.com>
2023-11-12 09:41:30 +0300

    javascript lessons-36
    javascript lessons-36

---
+++ Dars-36 CSS style'larini o'zgartirish/Dars-36.js
@@ -0,0 +1,7 @@
+// const heding = document.querySelector('h1')
+
+// heding.setAttribute('style', 'margin: 50px')
+
+// console.log(heding.style);
+// heding.style.color = 'red'
+// heding.style.margin = '50px'
