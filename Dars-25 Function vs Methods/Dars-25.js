870b62436c3af98c015d454a5af0d2e499178119
DostonLearn <149419882+CodeLearnUz@users.noreply.github.com>
2023-11-12 09:37:02 +0300

    javascript lessons-25
    javascript lessons-25

---
+++ Dars-25 Function vs Methods/Dars-25.js
@@ -0,0 +1,13 @@
+// function va Mthods
+
+const  ism = 'Doston'
+
+
+// functon
+const greet = () => 'Hello'
+const result = greet()
+console.log(result);
+
+// methods
+const res = ism.toUpperCase()
+console.log(res);
