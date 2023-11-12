111bc160b0dbd75b2475524fbbaf1f702e8ec616
DostonLearn <149419882+CodeLearnUz@users.noreply.github.com>
2023-11-12 09:32:13 +0300

    javascript lessons-12
    javascript lessons-12

---
+++ Dars-12 For loop/Dars-12.js
@@ -0,0 +1,11 @@
+// for loop
+
+// for(let i = 0; i < 10; i++){
+//     console.log('dosotn');
+// }
+
+// const names = ['doston', 'sardor', 'donyor']
+
+// for(let i = 0; i < names.length; i++){
+//     console.log(names[i]);
+// }
