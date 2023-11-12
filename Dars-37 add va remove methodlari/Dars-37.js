fd8992dba03c7a059d2e25d7d2c303efd15c89fd
DostonLearn <149419882+CodeLearnUz@users.noreply.github.com>
2023-11-12 09:41:46 +0300

    javascript lessons-37
    javascript lessons-37

---
+++ Dars-37 add va remove methodlari/Dars-37.js
@@ -0,0 +1,14 @@
+// const para = document.querySelectorAll('p')
+
+// para.forEach((p) =>{
+//     if(p.textContent.includes('succes')){
+//         p.classList.add('success')
+//     }
+//     if(p.textContent.includes('error')){
+//         p.classList.add('error')
+//     }
+// })
+
+// toggle
+// const hedding = document.querySelector('h1')
+// const hedding = document.querySelector('h1')
