4af1fec94a7643c70ace853eca7358ae8700fd29
DostonLearn <149419882+CodeLearnUz@users.noreply.github.com>
2023-11-12 09:32:32 +0300

    javascript lessons-13
    javascript lessons-13

---
+++ Dars-13 While loop/Dars-13.js
@@ -0,0 +1,18 @@
+// while
+
+const names = ['doston', 'sardor', 'donyor']
+
+// let i = 0;
+
+// while(i < names.length){
+//     console.log(names[i]);
+//     i++
+// }
+
+// do while
+
+// let i = 0
+// do{
+//         console.log(i);
+//         i++
+//     }while(i < 5)
