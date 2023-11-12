2d2987acdddced8668c63f3212d283044e7c62d3
DostonLearn <149419882+CodeLearnUz@users.noreply.github.com>
2023-11-12 09:34:52 +0300

    javascript lessons-19
    javascript lessons-19

---
+++ Dars-19 Switch Case/Dars-19.js
@@ -0,0 +1,33 @@
+const baho = 'D'
+
+switch(baho){
+    case 'A':
+        console.log('Sizning bahoyingiz A');
+        break
+    case 'B':
+        console.log('Sizning bahoyingiz B');
+        break
+    case 'C':
+        console.log('Sizning bahoyingiz C');
+        break
+    case 'D':
+        console.log('Sizning bahoyingiz D');
+        break
+    default:
+        console.log('Sizning bahoyingiz E');
+}
+
+
+
+
+// if(baho == 'A'){
+
+// }else if(baho == 'B'){
+
+// }else if(baho == 'C'){
+
+// }else if(baho == 'D'){
+
+// }else{
+
+// }
