82c6eb05035c3c777aab904c9de7245a0c7e35e0
DostonLearn <149419882+CodeLearnUz@users.noreply.github.com>
2023-11-12 09:33:24 +0300

    javascript lessons-15
    javascript lessons-15

---
+++ Dars-15 Else va else if/Dars-15.js
@@ -0,0 +1,12 @@
+// else if statements
+
+// const parol = 'password'
+
+
+// if(parol.length >= 12){
+//     console.log('Siznign parolingiz juda uzun !');
+// }else if(parol.length >= 8){
+//     console.log('Sizning parolingiz yetarlicha kuchli !');
+// }else{
+//     console.log('Sizning parolingiz yetarlicha kuchli emas !');
+// }
