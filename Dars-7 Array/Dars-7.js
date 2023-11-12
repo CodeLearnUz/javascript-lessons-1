38f8dbccd62306da315e66fb36442632112de42c
DostonLearn <149419882+CodeLearnUz@users.noreply.github.com>
2023-11-12 09:30:13 +0300

    javascript lessons-7
    javascript lessons-7

---
+++ Dars-7 Array/Dars-7.js
@@ -0,0 +1,26 @@
+// const mehmon1 = 'Azizbek'
+// const mehmon2 = 'MuhammadSaid'
+// const mehmon3 = 'Asror'
+// const mehmon4 = 'Donyor'
+
+// array => massiv
+
+// const mehmonlar = ['Azizbek', 'MuhammadSaid', 'Asror', 'Donyor']
+
+// mehmonlar[0] = 'Doston'
+// console.log(mehmonlar);
+
+// let numbers = [1,2,3,4,5,6,123]
+
+// property
+
+// const result = numbers.length
+// console.log(result);
+
+// let result = mehmonlar.indexOf('Donyor')
+// let result = mehmonlar.concat(numbers)
+
+// mehmonlar.push('Davlatbek')
+// mehmonlar.pop('Donyor')
+
+// console.log(mehmonlar);
