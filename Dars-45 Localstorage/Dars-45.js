da57b14d29fade5c9a323b97a9cd99a6ee3e0a59
DostonLearn <149419882+CodeLearnUz@users.noreply.github.com>
2023-11-12 09:44:24 +0300

    javascript lessons-45
    javascript lessons-45

---
+++ Dars-45 Localstorage/Dars-45.js
@@ -0,0 +1,41 @@
+// localstorage
+
+//  setItem
+// const names = ['Doston', 'Sardor', 'Donyor']
+// localStorage.setItem('names', JSON.stringify(names))
+// let yosh = 17
+
+// localStorage.setItem('name', ism)
+// localStorage.setItem('yosh', yosh)
+
+// getItem
+
+// const getNames = JSON.parse(localStorage.getItem('names'))
+// console.log(getNames);
+
+// const getYosh = localStorage.getItem('yosh')
+
+// console.log(getYosh);
+
+// clear
+
+// localStorage.clear()
+
+// const names = JSON.parse(localStorage.getItem('names')) ?
+// JSON.parse(localStorage.getItem('names')) : []
+
+// if(names.length > 0){
+//     names.forEach((name) => {
+//         console.log(name);
+//     })
+// }else{
+//     console.log("Ma'lumot yo'q");
+// }
+
+// const input = document.querySelector('input')
+// const p = document.querySelector('p')
+// const button = document.querySelector('button')
+
+// button.addEventListener('click', () => {
+//     localStorage.setItem('input value', input.value == p.textContent)
+// })
