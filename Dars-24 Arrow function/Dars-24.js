5adde60949b459c3ace7eeee210b015b3162da62
DostonLearn <149419882+CodeLearnUz@users.noreply.github.com>
2023-11-12 09:36:49 +0300

    javascript lessons-24
    javascript lessons-24

---
+++ Dars-24 Arrow function/Dars-24.js
@@ -0,0 +1,44 @@
+// const calcArea = (radius) => {
+//     return 3.14 * radius ** 2
+// }
+
+// const area = calcArea(5)
+// console.log(area);
+
+
+
+// const greet = function(ism){
+//     return `hello ${ism}`
+// }
+
+
+// const greet = (ism) => {
+//     return `Hello ${ism}`
+// }
+
+
+// const result = greet('Doston')
+
+// console.log(result);
+
+// const greet = (ism, familya) => `Hello ${ism} ${familya}`
+
+// const result = greet('Doston', 'Sultonov')
+
+// console.log(result);
+
+
+
+// const bill = function(products, tax){
+//     let total = 0
+
+//     for(let i = 0; i < products.length; i++){
+//         total += products[i] + products[i] * tax
+//     }
+
+//     return total
+// }
+
+// const result = bill([10, 13, 45], 0.2)
+
+// console.log(result);
